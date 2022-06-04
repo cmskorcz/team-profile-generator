0753021790432ae2a6da165aad0ff1b027080774
cmskorcz <chaseskorey@icloud.com>
2022-06-04 16:51:39 -0400

    added inquirer prompts

---
+++ index.js
@@ -0,0 +1,73 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Manager = require('./lib/Manager');
+
+const { addEngineer, addIntern} = require('./util/addEmployee')
+
+const teamArr = [];
+
+inquirer
+    .prompt([
+
+        {
+
+            type: 'text',
+            name: 'name',
+            message: `Please enter the Manager's name.`
+
+        },
+
+        {
+
+            type: 'text',
+            name: 'id',
+            message: `Please enter the Manager's id.`
+
+        },
+
+        {
+
+            type: 'text',
+            name: 'email',
+            message: `Please enter the Manager's email.`
+
+        }, 
+
+        {
+
+            type: 'text',
+            name: 'office',
+            message: `Please enter the Manager's office number.`
+
+        },
+
+        {
+
+            type: 'list',
+            message: `Would you like to add more members, or finish team?`,
+            name: 'action',
+            choices: ['Add Engineer', 'Add Intern', 'Finish']
+
+        }
+    ])
+    .then(({ name, id, email, office, action }) => {
+
+        const manager = new Manager(name, id, email, office);
+
+        teamArr.push(manager);
+
+        if (action === 'Add Engineer') {
+
+            addEngineer(teamArr)
+
+        } else if (action === 'Add Intern') {
+
+            addIntern(teamArr)
+
+        } else {
+
+            // finish
+
+        }
+    })
