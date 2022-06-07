29251348f24d9b965ddd300903477aadd1bb18e2
Chase Skorczewski <chaseskorey@icloud.com>
2022-06-07 12:52:30 -0400

    added minimal boostrap styling

--- src/html-template.js
+++ src/html-template.js
@@ -1,26 +1,27 @@
 const generateManager = teamArr => {
 
-    return `
-        <div>
-        ${teamArr
-            .filter(({ officeNumber }) => officeNumber)
-            .map(({ name, id, email, officeNumber, getRole }) => {
+    return teamArr
+        .filter(({ officeNumber }) => officeNumber)
+        .map(({ name, id, email, officeNumber, getRole }) => {
 
-                return `
+            return `
+            <div class="card">
+                <div class="card-header">
                     <h2>${name}</h2>
                     <h3>${getRole()}</h3>
-                    <p>${id}</p>
-                    <p>${email}</p>
-                    <p>${officeNumber}</p>
-                
-                `
-            })
-            .join('')
-        }
-        </div>
-    
-    `
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">Office: ${officeNumber}</li>
+                </ul>
+            </div>
+            `
+        })
+        .join('')
 }
+
+
 
 const generateEngineer = teamArr => {
 
@@ -30,12 +31,16 @@ const generateEngineer = teamArr => {
         .map(({ name, id, email, github, getRole }) => {
 
             return `
-            <div>
-                <h2>${name}</h2>
-                <h3>${getRole()}</h3>
-                <p>${id}</p>
-                <p>${email}</p>
-                <p>${github}</p>
+            <div class="card">
+                <div class="card-header">
+                    <h2>${name}</h2>
+                    <h3>${getRole()}</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">GitHub: <a href="https://github.com/${github}" target="_blank">${github}</a></li>
+                </ul>
             </div>
             `
         })
@@ -49,12 +54,16 @@ const generateIntern = teamArr => {
         .map(({ name, id, email, school, getRole }) => {
 
             return `
-            <div>
-                <h2>${name}</h2>
-                <h3>${getRole()}</h3>
-                <p>${id}</p>
-                <p>${email}</p>
-                <p>${school}</p>
+            <div class="card">
+                <div class="card-header">
+                    <h2>${name}</h2>
+                    <h3>${getRole()}</h3>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID: ${id}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${email}">${email}</a></li>
+                    <li class="list-group-item">School: ${school}</li>
+                </ul>
             </div>
             `
         })
@@ -73,18 +82,19 @@ module.exports = templateData => {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="style.css"
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
     </head>
 
     <body>
         <header>
-            <h1>Team Members</h1>
+            <h1 class="text-center bg-danger">Team Members</h1>
         </header>
-        <main>
+        <main class="container">
             ${generateManager(templateData)}
             ${generateEngineer(templateData)}
             ${generateIntern(templateData)}
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
     </body>
     </html>
     `
